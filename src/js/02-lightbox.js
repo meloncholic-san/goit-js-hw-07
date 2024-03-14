52a141c02743cb20d880823927252dae2b8fbf47
Panchenko Denis <Pandenisko@gmail.com>
2024-03-14 13:30:38 +0200

    done
    done

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+function createGalleryItems(images) {
+    return images.map(image => 
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+        <img class = "gallery__image" src = "${image.preview}" alt="${image.description}"> </img> </a>
+        </li>`
+    ).join("");
+}
 
-console.log(galleryItems);
+function onGalleryClick(event){
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return;
+    modalImage.open();
+}
+const galleryRef = document.querySelector('.gallery');
+
+const galleryItemMarkup = createGalleryItems(galleryItems);
+galleryRef.innerHTML = galleryItemMarkup;
+
+const modalImage = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+galleryRef.addEventListener('click', onGalleryClick);
+
